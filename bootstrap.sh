802c18962efcf29c81a7575484c49872f3f28072
Jamin X. Chen <chenxm35@gmail.com>
2014-08-04 21:23:23 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,14 +17,9 @@ function clean() {
 
 function doIt() {
     for i in $(ls -a); do 
-	if [ $i != '.' -a $i != '..' -a $i != '.git' -a $i != '.DS_Store' -a $i != 'bootstrap.sh' -a $i != 'README.md' -a $i != '.gitignore' -a $i != '.gitmodules' ]; then 
-        if [ $(uname) == "Darwin" ]; then
-            echo "$i"
-	        gcp -alrf "$i" "$HOME/"
-        else
-            echo "$i"
-            cp -arf "$i" "$HOME/"
-        fi
+	if [ $i != '.' -a $i != '..' -a $i != '.git' -a $i != '.DS_Store' -a $i != 'bootstrap.sh' -a $i != 'README.md' -a $i != '.gitignore' -a $i != '.gitmodules' ]; then
+        echo "$i"
+        cp -afR "$i" "$HOME/"
 	fi
     done
 }
